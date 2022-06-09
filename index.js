7d46ca815e9bdeefab9b38f3d8d780b768e9a437
livramateus <livra.matheus@gmail.com>
2022-06-08 21:51:30 -0300

    Fixed stage sound bug

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-let stageList = [
-    'Barriers',
-    'Hangar',
-    'Carrier',
-    'MiningSite',
-    'ColdFields',
-    'ControlRoom',
-    'GinarzasHq',
-];
-
-// Temporary stage randomizer
-let stgName = stageList[parseInt(Math.random() * stageList.length)];
-
-const Stage = await import(`./assets/Stages/${stgName}.js`);
-
 import Enemy from "./assets/Characters/Gonza.js"
 import Player from "./assets/Characters/Thunder.js"
 import Fight from "./events/Fight.js";
 
-const Fight1 = new Fight(Player, Enemy, Stage.default);
-Fight1.animate();
+canvas.addEventListener('click', async () => {
+    let stageList = [
+        'Barriers',
+        'Hangar',
+        'Carrier',
+        'MiningSite',
+        'ColdFields',
+        'ControlRoom',
+        'GinarzasHq',
+    ];
+    
+    // Temporary stage randomizer
+    let stgName = stageList[parseInt(Math.random() * stageList.length)];
+    const Stage = await import(`./assets/Stages/${stgName}.js`);
+    
+    const Fight1 = new Fight(Player, Enemy, Stage.default);
+    Fight1.animate();
+});
