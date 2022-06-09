64f1a60c6a51ccfd4538180651d7002292f3bab0
livramateus <livra.matheus@gmail.com>
2022-06-08 22:00:50 -0300

    Fixed issue when canvas was interacted twice

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ import Player from "./assets/Characters/Thunder.js"
 import Fight from "./events/Fight.js";
 
 canvas.addEventListener('click', async () => {
-    let stageList = [
-        'Barriers',
-        'Hangar',
-        'Carrier',
-        'MiningSite',
-        'ColdFields',
-        'ControlRoom',
-        'GinarzasHq',
-    ];
-    
-    // Temporary stage randomizer
-    let stgName = stageList[parseInt(Math.random() * stageList.length)];
-    const Stage = await import(`./assets/Stages/${stgName}.js`);
-    
-    const Fight1 = new Fight(Player, Enemy, Stage.default);
-    Fight1.animate();
+    if (!canvas.interacted) {
+        let stageList = [
+            'Barriers',
+            'Hangar',
+            'Carrier',
+            'MiningSite',
+            'ColdFields',
+            'ControlRoom',
+            'GinarzasHq',
+        ];
+        
+        // Temporary stage randomizer
+        let stgName = stageList[parseInt(Math.random() * stageList.length)];
+        const Stage = await import(`./assets/Stages/${stgName}.js`);
+        
+        const Fight1 = new Fight(Player, Enemy, Stage.default);
+        Fight1.animate();
+
+        canvas.interacted = true;
+    }
 });
